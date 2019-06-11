40df02e7825df8bc0a015fb69ab17798c3dcfe9b
Eugenia Leung <eugenia_leung316@yahoo.com>
2019-06-11 00:52:19 -0700

    implemented webhooks

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,3 +1,6 @@
+const _ = require("lodash");
+const { Path } = require("path-parser");
+const { URL } = require("url");
 const mongoose = require("mongoose");
 const requireLogin = require("../middlewares/requireLogin");
 const requireCredits = require("../middlewares/requireCredits");
@@ -7,9 +10,33 @@ const surveyTemplate = require("../services/emailTemplates/surveyTemplate");
 const Survey = mongoose.model("surveys");
 
 module.exports = app => {
-  // app.get("/api/surveys/thanks", (req, res) => {
-  //   res.send("Thanks for voting!");
-  // });
+  app.get("/api/surveys/thanks", (req, res) => {
+    res.send("Thanks for voting!");
+  });
+
+  app.post("/api/surveys/webhooks", (req, res) => {
+    // console.log(req.body);
+    // res.send({});
+    const events = _.map(req.body, ({ email, url }) => {
+      const pathname = new URL(url).pathname;
+      const p = new Path("/api/surveys/:surveyId/:choice");
+      // console.log(p.test(pathname));
+      const match = p.test(pathname);
+      if (match) {
+        return {
+          email,
+          surveyId: match.surveyId,
+          choice: match.choice
+        };
+      }
+    });
+    // console.log(events);
+    const compactEvents = _.compact(events);
+    const uniqueEvents = _.uniqBy(compactEvents, "email", "surveyId");
+    console.log(uniqueEvents);
+
+    res.send({});
+  });
 
   app.post("/api/surveys", requireLogin, requireCredits, async (req, res) => {
     const { title, subject, body, recipients } = req.body;
